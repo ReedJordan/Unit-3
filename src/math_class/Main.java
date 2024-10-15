e1b748fc2086f971716e1acf6161132124eba7fd
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-10-15 14:59:53 -0700

    Homework

--- src/math_class/Main.java
+++ src/math_class/Main.java
@@ -16,21 +16,47 @@ public class Main {
         // ClassName.methodName(); e.g. Math.random()
         // ClassName.staticVariableName(); e.g. Math.PI
 
+        double myPI = Math.PI;
+
+
         // abs
+        int num1 = -2;
+        System.out.println(Math.abs(num1));
 
         // pow
+        double num2 = Math.pow(3,5);
+        System.out.println((int)num2);
 
         // sqrt
+        double num3 = Math.sqrt(81);
+        System.out.println(num3);
+        System.out.println(Math.pow(num3,2));
 
 
+        //create a random integer in the range [0,1)
+        double random1 = (int) Math.random();
+        System.out.println(random1);
 
-        //create a random number in the range [0,1)
-
-        //create a random number in the range [0, n)
+        //create a random integer in the range [0, n)
+        //Ex: n = 6
+        double random2 = (int) (Math.random() * 6);
+        System.out.println(random2);
+        //in general, just multiply by n
 
         // create a random number in the range [0,n]
+        //Ex: n = 6
+        double random3 = (int) (Math.random()*7);
 
-        // create a random number in the range [m,n]
+        //For Homework
+
+        // 1) Create a random number in the range [m,n]
+
+        // 2) Create a random negative integer in the range [-m,0]
+
+        // 3) Create a random negative integer in the range [-m,-n]
+
+        // 4) Create a random negative integer in the range [-m,n]
+
 
 
 
