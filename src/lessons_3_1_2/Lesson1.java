fa9229a7f0edac6db9907ad3a393877f31029880
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-10-17 14:43:23 -0700

    Lessons completed

--- src/lessons_3_1_2/Lesson1.java
+++ src/lessons_3_1_2/Lesson1.java
@@ -15,7 +15,10 @@ public class Lesson1 {
         //
 
         // is x greater than y? store result in boolean variable
-
+        boolean xGTy = x > y;
+        System.out.println(xGTy); //it's false
+        String name = "Henry";
+        String name2 = new String("Henry"); //nonono, not good
 
         //which two rectangles are the same?
         Rectangle rect1 = new Rectangle(1,3);
@@ -23,9 +26,10 @@ public class Lesson1 {
         Rectangle rect3 = new Rectangle(5,7);
 
         //does this work? why or why not? If not, fix it
-        boolean oneAndTwo = rect1 == rect2;
-        boolean oneAndThree = rect1 == rect3;
-        boolean twoAndThree = rect2 == rect3;
+        //NO! We need to use .equals() for objects. == compares the references
+        boolean oneAndTwo = rect1.equals(rect2); //does not work as expected because it compares memory addresses
+        boolean oneAndThree = rect1.equals(rect3);
+        boolean twoAndThree = rect2.equals(rect3);
         System.out.printf("Rectangles 1 and 2: %b\nRectangles 1 and 3: " +
                 "%b\nRectangles 2 and 3: %b",oneAndTwo,oneAndThree,twoAndThree);
 
--- src/lessons_3_1_2/Lesson2.java
+++ src/lessons_3_1_2/Lesson2.java
@@ -1,18 +1,33 @@
 package lessons_3_1_2;
+import java.util.Scanner;
 
 public class Lesson2 {
     public static void main(String[] args) {
         // we only printed true/false before, but let's use those outputs
         // to change the flow of the program
-
+        Scanner input = new Scanner(System.in);
         // Ask a user for their favorite number
+        System.out.println("What is your favorite number?");
+        int favNumber = input.nextInt();
 
         // if it is above 12, print "That's a large number!"
         // if it is 12 or lower, print "That's small number"
+        if (favNumber > 12){
+            System.out.println("That's a large number!");
+        }
+        if (favNumber < 12){
+            System.out.println("That's a small number.");
+        }
+
+        //alternative way of doing this
+
+        if (favNumber > 12){
+            System.out.println("That's a large number!");
+        }else{
+            System.out.println("That's a small number.");
+        }
 
         // Go to the rectangle class and implement the isSquare method
         // following the JavaDoc specs
-
-
     }
 }
