33b95d84d8a7cc4f66763911f374054e4b7b0454
jordanr_wlhs <jordanr@wlhs.wlwv.k12.or.us>
2024-10-25 14:58:04 -0700

    Homework

---
+++ src/Practice/practice.java
@@ -0,0 +1,18 @@
+package Practice; //don't change this
+
+public class practice {
+    public static void main(String[] args) {
+        //remove the // to run the methods
+
+        //problemOne()
+        //problemTwo()
+    }
+
+    public static void problemOne(){
+        // code here
+    }
+
+    public static void problemTwo(){
+        //code here
+    }
+}
